0b47774036f1c43a38eab4e93082f89e1b87b6b7
Mikko Kemppainen <mikko.kemppainen@gredi.fi>
2023-08-23 23:28:22 +0300

    Create some towers and enemies that avoid them
    Enemies avoid walking straight into other entities.
    They can step sideways on something still.

--- src/entities/enemy-entity.ts
+++ src/entities/enemy-entity.ts
@@ -1,8 +1,44 @@
 import { Entity } from 'entities/entity';
+import { grid } from '../main.ts';
 
-export const enemyTick = (entity: Entity) => {
+/**
+ * Moves entity if it's enemy
+ */
+export const moveEnemy = (selfEntity: Entity, allEntities: Entity[]) => {
+  if (selfEntity.name !== 'enemy') {
+    return selfEntity;
+  }
+  const newGridX = selfEntity.gridX + 1;
+  const collidedWithEntity = allEntities.find((ent) => {
+    // Avoids other enemies that would move out of the way, but maybe it doesn't matter
+    return selfEntity !== ent && newGridX === ent.gridX && selfEntity.gridY === ent.gridY;
+  });
+  if (collidedWithEntity) {
+    // Can't move forward so move to the side. Not checking if there is anything for now
+    return {
+      ...selfEntity,
+      gridY: selfEntity.gridY + 1,
+    };
+  }
   return {
-    ...entity,
-    gridX: entity.gridX + 1,
+    ...selfEntity,
+    gridX: newGridX,
   };
+};
+
+export const createEnemyOnCoast = () => {
+  const y = Math.ceil(Math.random() * (grid.rowCount - 1));
+  const enemy: Entity = {
+    color: 'red',
+    gridX: 11,
+    gridY: y,
+    name: 'enemy',
+    stats: {
+      hp: 5,
+      attack: 1,
+      defence: 1,
+      speed: 0,
+    },
+  };
+  return enemy;
 };
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,64 @@
 import { initControls } from 'controller';
 import { Grid } from 'grid';
-import { enemyTick } from 'entities/enemy-entity';
-import { getEntitiesState, getSystemState, updateEntitiesState, updateSystemState } from 'state';
-import { renderMenu } from 'ui/main-menu';
+import {
+  getEntitiesState,
+  getState,
+  getSystemState,
+  updateEntitiesState,
+  updateSystemState,
+} from 'state';
+import { createEnemyOnCoast, moveEnemy } from './entities/enemy-entity.ts';
+import { randomInt } from './math.ts';
+import { renderMenu } from './ui/main-menu.ts';
 
 export const grid = new Grid();
 
-initControls();
+export const initEntities = () => {
+  const coastLineX = 13;
+  const rightEdgeX = grid.colCount - 7; // TODO why is right edge of grid not visible
+  const makeTower = () => {
+    return {
+      color: 'purple',
+      gridX: randomInt(coastLineX, rightEdgeX),
+      gridY: randomInt(1, grid.rowCount - 1),
+      name: 'tower',
+      stats: {
+        hp: 5,
+        attack: 1,
+        defence: 1,
+        speed: 0,
+      },
+    };
+  };
+  const towers = [...Array(40)].map(() => makeTower());
+  updateEntitiesState([...getEntitiesState(), ...towers]);
+};
 
 const sleep = async () => {
   await new Promise((resolve) => setTimeout(resolve, 266));
 };
 
-const runTicks = () => {
-  console.log(getEntitiesState());
-  const newEntities = getEntitiesState().map((e) => enemyTick(e));
-  updateEntitiesState(newEntities);
+/**
+ * Run game systems that make up the game logic. I.e. run ticks.
+ */
+export const runGameSystems = () => {
+  updateEntitiesState(getEntitiesState().map((e) => moveEnemy(e, getEntitiesState())));
+  if (getState().system.timer % 5 === 0) {
+    updateEntitiesState([...getEntitiesState(), createEnemyOnCoast()]);
+  }
 };
 
-(async () => {
-  renderMenu(grid);
+export const runGameLoop = async () => {
   for (;;) {
     const system = getSystemState();
     updateSystemState({ ...system, timer: system.timer + 1 });
-    getSystemState().timer % 10 === 0 && console.log('ticking along', getSystemState().timer);
-    runTicks();
+    runGameSystems();
     grid.genEls();
     await sleep();
   }
-})();
+};
+
+renderMenu(grid);
+initControls();
+initEntities();
+runGameLoop();
