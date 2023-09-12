636109a95f66cece5df9f07730a8423c219019fd
mikkael3 <niko.heikkinen3@gmail.com>
2023-09-12 21:16:42 +0300

    village sprites

--- src/entities/village.ts
+++ src/entities/village.ts
@@ -3,13 +3,15 @@ import { getState } from '../state.ts';
 
 export const renderVillage = (grid: Grid) => {
   const village = getState().village;
-  const villageElement = document.createElement('div');
-  villageElement.style.backgroundColor = 'dimgray';
-  villageElement.style.gridArea = `${village.y}/${village.x}/${village.y + village.height}/${
+  const element = document.createElement('div');
+  element.style.gridArea = `${village.y}/${village.x}/${village.y + village.height}/${
     village.x + village.width
   }`;
-  grid.entityGrid.appendChild(villageElement);
-  return villageElement;
+  element.style.background = 'transparent';
+  element.style.backgroundImage = "url('/v.png')";
+  element.style.backgroundSize = '32px';
+  grid.entityGrid.appendChild(element);
+  return element;
 };
 
 export const getVillageCoordinates = () => {
