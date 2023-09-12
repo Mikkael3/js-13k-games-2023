91d411e355783e42b0bad3b831203c49711ea819
mikkael3 <niko.heikkinen3@gmail.com>
2023-09-13 00:52:15 +0300

    awesome end screen

--- src/ui/end-screen.ts
+++ src/ui/end-screen.ts
@@ -7,7 +7,8 @@ export const renderEndScreen = (grid: Grid, endText: string) => {
   grid.uiHtmlGrid.innerHTML = '';
   const container = document.createElement('div');
   container.textContent = endText;
-  container.style.backgroundColor = 'white';
+  container.style.backgroundColor = 'black';
+  container.style.color = 'white';
   container.style.gridArea = `1/1/28/49`;
   container.style.display = 'grid';
   container.style.gridTemplateColumns = `repeat(${1}, 1fr)`;
@@ -15,6 +16,9 @@ export const renderEndScreen = (grid: Grid, endText: string) => {
   grid.uiHtmlGrid.appendChild(container);
   const button = document.createElement('button');
   button.textContent = 'Start a new game click here';
+  button.style.background = 'red';
+  button.style.fontSize = '20px';
+
   container.appendChild(button);
   button.onclick = resetState;
 };
